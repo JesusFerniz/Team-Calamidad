a7c8b11a604403dee5a6262c4157b5edcc0500d4
Jesús Eduardo Ferniz Cavazos <jesus_ferniz@hotmail.com>
2020-06-08 15:17:46 -0600

    Scripts: Move, Coins and GameManager

---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text coinText;
+    int coins;
+
+    public void AddCoin()
+    {
+        coins = coins + 10;
+        Debug.Log("Puntuación: " + coins);
+
+        coinText.text = "Puntuación: " + coins;
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void GameWin()
+    {
+        Debug.Log("Game Over");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
